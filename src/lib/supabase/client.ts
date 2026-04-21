b7068bff3d30e3ef696989732b439951aea7efae
Ricardo <contato@codigocomercial.com.br>
2026-04-21 20:04:22 -0300

    fix: env without secrets

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    'https://kqaoycehypzeqvxrewzl.supabase.co',
-    'sb_publishable_27WgSEtNN1z2mo_Whh3n5Q__P6qDViT'
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   )
 }
