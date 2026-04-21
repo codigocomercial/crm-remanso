4dd789c4715229040e007aa3a5937a9e19b65239
Ricardo <contato@codigocomercial.com.br>
2026-04-21 15:54:19 -0300

    fix: hardcode supabase credentials temporarily

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    'https://kqaoycehypzeqvxrewzl.supabase.co',
+    'sb_publishable_27WgSEtNN1z2mo_Whh3n5Q__P6qDViT'
   )
 }
